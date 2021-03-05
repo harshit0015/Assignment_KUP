a4d073d004f7d0b8e037beff5362541e97ecf1ee
Harshit Gupta <harshit.gupta@knoldus.com>
2021-03-05 20:54:37 +0530

    Submitted Assignment

---
+++ Assignment_2_Lambda/src/main/java/assignment_2/lambdas/GeneratePrimesInRangeInterface.java
@@ -0,0 +1,7 @@
+package assignment_2.lambdas;
+import java.util.ArrayList;
+import java.util.List;
+
+public interface GeneratePrimesInRangeInterface {
+    List<Integer>  filterOutPrimes(ArrayList<Integer> inputNumbers);
+}
---
+++ Assignment_2_Lambda/src/main/java/assignment_2/lambdas/GenerateRandomNumbersInterface.java
@@ -0,0 +1,5 @@
+package assignment_2.lambdas;
+import java.util.ArrayList;
+public interface GenerateRandomNumbersInterface {
+    public ArrayList<Integer> randomInput();
+}
