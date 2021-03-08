2e28200bee89a5ac5c6e3a7f6e07712915c0afc2
Harshit Gupta <harshit.gupta@knoldus.com>
2021-03-08 22:49:10 +0530

    Assignment Submitted

---
+++ Assignment_lambda_2/ReadMe.md
@@ -0,0 +1,3 @@
+
+
+Write a lambda expression that accepts a long value and returns a next even number.
---
+++ Assignment_lambda_2/src/main/java/com/knoldus/Even_number_next.java
@@ -0,0 +1,9 @@
+package com.knoldus;
+
+import java.util.function.Function;
+@FunctionalInterface
+  public interface Even_number_next
+ {
+
+    Function<Integer,Integer> function();
+  }
